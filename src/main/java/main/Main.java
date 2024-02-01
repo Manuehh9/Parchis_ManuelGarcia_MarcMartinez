049fbae717964e1c6e2ec508c59522c63c086a6b
Manuehh9 <120485899+Manuehh9@users.noreply.github.com>
2024-02-01 15:20:47 +0100

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,9 +7,3 @@ public class Main {
 
 	}
 }
-git init
-$ git add .
-$ git commit -m "first commit"
-$ git remote add origin https://github.com/NOMBRE_USUARIO/NOMBRE_PROYECTO.git
-$ git branch -M main
-$ git push -f -u origin main

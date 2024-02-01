903f1b1af190f23b34be4d6134f97295cbc0a6e5
Manuel Garcia Rodriguez <manuel.garcia.rodriguez.010@gmail.com>
2024-02-01 15:06:10 +0100

    Proyecto vacio

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
+git init
+$ git add .
+$ git commit -m "first commit"
+$ git remote add origin https://github.com/NOMBRE_USUARIO/NOMBRE_PROYECTO.git
+$ git branch -M main
+$ git push -f -u origin main
